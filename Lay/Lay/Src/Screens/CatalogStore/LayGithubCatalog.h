5946f14b24953498f74c42be2bd4bee674fd8d92
Rene Kollmorgen <kolle@gmx.net>
2014-05-02 20:14:15 +0200

    Close #1

---
+++ Lay/Lay/Src/Screens/CatalogStore/LayGithubCatalog.h
@@ -0,0 +1,13 @@
+//
+//  LayGithubCatalog.h
+//  KEEMI
+//
+//  Created by Rene Kollmorgen on 02.05.14.
+//  Copyright (c) 2014 Rene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LayGithubCatalog : NSObject
+
+@end
---
+++ Lay/Lay/Src/Screens/CatalogStore/LayGithubCatalog.m
@@ -0,0 +1,13 @@
+//
+//  LayGithubCatalog.m
+//  KEEMI
+//
+//  Created by Rene Kollmorgen on 02.05.14.
+//  Copyright (c) 2014 Rene. All rights reserved.
+//
+
+#import "LayGithubCatalog.h"
+
+@implementation LayGithubCatalog
+
+@end
